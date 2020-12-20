64e6c8d5ba719cd2eaec838d06c4cf90d2792d72
hondrostrong-official <76270734+hondrostrong-official@users.noreply.github.com>
2020-12-20 05:26:57 +0200

    2

---
+++ js/mx3.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+
+    window.countryList = {
+        mx: {
+            name: 'México',
+            code: 'mx',
+            price: 749,
+            oldPrice: 1498,
+            labelPrice: 'MXN',
+            phoneHelper: 'Por ejemplo +52 12345678',
+            nameHelper: 'Alejandro Rodriguez',
+            nameError: 'Incorrectamente declarado el nombre',
+            phoneError: 'Teléfono que aparece incorrectamente',
+            countryError: 'Incorrectamente especificado País',
+            isDefault: true
+        }
+    };
+})();
